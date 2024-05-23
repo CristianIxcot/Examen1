2e8300915677b12e8a852a05bb432ceb95af3f8e
quiss <quiss@MSI>
2024-05-23 09:26:30 -0600

    Agregar archivos de proyecto.

---
+++ Examen1/Libro.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Examen1
+{
+    public class Libro
+    {
+        [Required(ErrorMessage = "EL Titulo es Requerido")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "EL Autor es Requerido")]
+        public string Autor { get; set; }
+        [Required(ErrorMessage = "EL Genero es Requerido")]
+        public string Genero { get; set; }
+        [Required(ErrorMessage = "EL Año de publicacion es Requerido")]
+        public int Anio { get; set; }
+
+        public Libro()
+        {
+            Titulo = string.Empty;
+            Autor = string.Empty;
+            Genero = string.Empty;
+            Anio = 0;
+
+        }
+    }
+}
---
+++ Examen1/LibrosElectronicos.cs
@@ -0,0 +1,8 @@
+﻿namespace Examen1
+{
+    public class LibrosElectronicos:Libro
+    {
+        public string Formato { get; set; }
+        public int tamano { get; set; }
+    }
+}
